dfda44f8c167f43d1e7f71ed134c8d4287a312ef
wilkie <wilkie05@gmail.com>
2011-08-27 19:58:30 -0400

    Added grammar documentation for ShiftExpr

--- syntax/shiftexprunit.d
+++ syntax/shiftexprunit.d
@@ -14,6 +14,15 @@ import syntax.nodes;
 
 import syntax.addexprunit;
 
+/*
+
+	ShiftExpr => ShiftExpr >> AddExpr
+	           | ShiftExpr << AddExpr
+			   | ShiftExpr >>> AddExpr
+			   | AddExpr
+
+*/
+
 class ShiftExprUnit : ParseUnit {
 	override bool tokenFound(Token current) {
 		switch (current.type) {
