9ad55592a3f792b7dad4e011c7fd6e3908e09c1e
wilkie <wilkie05@gmail.com>
2011-08-27 19:53:54 -0400

    Added grammar documentation for XorExpr

--- syntax/xorexprunit.d
+++ syntax/xorexprunit.d
@@ -14,6 +14,13 @@ import syntax.nodes;
 
 import syntax.andexprunit;
 
+/*
+
+	XorExpr => XorExpr ^ AndExpr
+             | AndExpr	
+
+*/
+
 class XorExprUnit : ParseUnit {
 	override bool tokenFound(Token current) {
 		switch (current.type) {
