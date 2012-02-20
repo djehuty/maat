d7135f00f779c52ebf9bb6ee9ea765d63f856e92
wilkie <wilkie05@gmail.com>
2012-02-20 17:30:20 -0500

    FunctionNode holds the name of the function (if there is one)

--- ast/function_node.d
+++ ast/function_node.d
@@ -4,17 +4,25 @@ import ast.statement_node;
 
 class FunctionNode {
 private:
+	char[] _name;
 	StatementNode[] _bodyBlock;
 	StatementNode[] _inBlock;
 	StatementNode[] _outBlock;
 
 public:
-	this(StatementNode[] bodyBlock,
+	this(char[] name,
+	     StatementNode[] bodyBlock,
 	     StatementNode[] inBlock,
 		 StatementNode[] outBlock) {
+
+		_name = name.dup;
 		_bodyBlock = bodyBlock.dup;
 		_inBlock = inBlock.dup;
 		_outBlock = outBlock.dup;
+	}
+
+	char[] name() {
+		return _name.dup;
 	}
 
 	StatementNode[] bodyBlock() {
