5ce1120d8012665397f79f372b66a8ee6f245bce
wilkie <wilkie05@gmail.com>
2012-05-13 03:26:40 -0400

    Creates StaticDisambiguationUnit.
    This unit is necessary to decide, upon seeing the 'static' keyword,
    which type of grammar it will be. It looks at the next token to decide
    between:
    
    StaticIfStatement (sees an 'if')
    StaticAssertStatement (sees an 'assert')
    StaticConstructorStatement (sees 'this')
    StaticDestructorStatement (sees a '~')
    StaticAttribute (sees anything else... will go to AttributeUnit eventually)

--- syntax/declaration_unit.d
+++ syntax/declaration_unit.d
@@ -6,8 +6,11 @@
 module syntax.declaration_unit;
 
 import syntax.pragma_statement_unit;
+import syntax.attribute_unit;
 import syntax.debug_statement_unit;
-import syntax.static_statement_unit;
+import syntax.static_disambiguation_unit;
+import syntax.static_if_statement_unit;
+import syntax.static_assert_statement_unit;
 import syntax.type_declaration_unit;
 import syntax.class_declaration_unit;
 import syntax.interface_declaration_unit;
@@ -31,8 +34,7 @@ private:
 	Lexer  _lexer;
 	Logger _logger;
 
-	DeclarationNode.Type _type;
-	Object _node;
+  DeclarationNode _declaration;
 
 public:
 	this(Lexer lexer, Logger logger) {
@@ -47,7 +49,7 @@ public:
 			token = _lexer.pop();
 		} while (tokenFound(token));
 
-		return new DeclarationNode(_type, _node);
+    return _declaration;
 	}
 
 	bool tokenFound(Token token) {
@@ -66,7 +68,34 @@ public:
 				_logger.error(_lexer, token, "Module declaration should be on one of the first lines of the file.");
 				break;
 
+			case Token.Type.Pragma:
+				auto stmt = (new PragmaStatementUnit(_lexer, _logger)).parse;
+				break;
+
+			// Static
+			case Token.Type.Static:
+        auto staticVariant = (new StaticDisambiguationUnit(_lexer, _logger)).parse;
+
+        if (staticVariant == StaticDisambiguationUnit.StaticVariant.StaticIf) {
+//          _declaration = (new StaticIfStatementUnit(_lexer, _logger)).parse;
+        }
+        else if (staticVariant == StaticDisambiguationUnit.StaticVariant.StaticAssert) {
+//          _declaration = (new StaticAssertStatementUnit(_lexer, _logger)).parse;
+        }
+        else if (staticVariant == StaticDisambiguationUnit.StaticVariant.StaticConstructor) {
+//          _declaration = (new StaticConstructorStatementUnit(_lexer, _logger)).parse;
+        }
+        else if (staticVariant == StaticDisambiguationUnit.StaticVariant.StaticDestructor) {
+//          _declaration = (new StaticDestructorStatementUnit(_lexer, _logger)).parse;
+        }
+        else if (staticVariant == StaticDisambiguationUnit.StaticVariant.StaticAttribute) {
+          _lexer.push(token);
+          _declaration = (new AttributeUnit(_lexer, _logger)).parse;
+        }
+				break;
+
 			// Attribute Specifier
+      case Token.Type.Const:
 			case Token.Type.Synchronized:
 			case Token.Type.Deprecated:
 			case Token.Type.Final:
@@ -75,26 +104,21 @@ public:
 			case Token.Type.Scope:
 			case Token.Type.Private:
 			case Token.Type.Public:
+			case Token.Type.Package:
 			case Token.Type.Protected:
 			case Token.Type.Export:
 			case Token.Type.Extern:
 			case Token.Type.Align:
-				break;
-
-			case Token.Type.Pragma:
-				auto stmt = (new PragmaStatementUnit(_lexer, _logger)).parse;
-				break;
-
-			// Static
-			case Token.Type.Static:
-				auto stmt = (new StaticStatementUnit(_lexer, _logger)).parse;
-				break;
+        _lexer.push(token);
+        _declaration = (new AttributeUnit(_lexer, _logger)).parse;
+        break;
 
 			// Import Declaration
 			case Token.Type.Import:
 				auto import_path = (new ImportDeclarationUnit(_lexer, _logger)).parse;
-				_type = DeclarationNode.Type.ImportDeclaration;
-				_node = new ImportDeclarationNode(import_path);
+				auto type = DeclarationNode.Type.ImportDeclaration;
+				auto node = new ImportDeclarationNode(import_path);
+        _declaration = new DeclarationNode(type, node);
 				break;
 
 			// Enum Declaration
@@ -112,8 +136,9 @@ public:
 
 			// Class Declaration
 			case Token.Type.Class:
-				_type = DeclarationNode.Type.ClassDeclaration;
-				_node = (new ClassDeclarationUnit(_lexer, _logger)).parse;
+				auto type = DeclarationNode.Type.ClassDeclaration;
+				auto node = (new ClassDeclarationUnit(_lexer, _logger)).parse;
+        _declaration = new DeclarationNode(type, node);
 				break;
 
 			// Interface Declaration
@@ -129,14 +154,16 @@ public:
 
 			// Constructor Declaration
 			case Token.Type.This:
-				_type = DeclarationNode.Type.ConstructorDeclaration;
-				_node = (new ConstructorDeclarationUnit(_lexer, _logger)).parse;
+				auto type = DeclarationNode.Type.ConstructorDeclaration;
+				auto node = (new ConstructorDeclarationUnit(_lexer, _logger)).parse;
+        _declaration = new DeclarationNode(type, node);
 				break;
 
 			// Destructor Declaration
 			case Token.Type.Cat:
-				_type = DeclarationNode.Type.DestructorDeclaration;
-				_node = (new DestructorDeclarationUnit(_lexer, _logger)).parse;
+				auto type = DeclarationNode.Type.DestructorDeclaration;
+				auto node = (new DestructorDeclarationUnit(_lexer, _logger)).parse;
+        _declaration = new DeclarationNode(type, node);
 				break;
 
 			// Version Block
@@ -171,9 +198,15 @@ public:
 				_lexer.push(token);
 				auto decl = (new TypeDeclarationUnit(_lexer, _logger)).parse;
 				if (decl.type == TypeDeclarationNode.Type.FunctionDeclaration) {
-					_node = decl.node;
-					_type = DeclarationNode.Type.FunctionDeclaration;
+					auto node = decl.node;
+					auto type = DeclarationNode.Type.FunctionDeclaration;
+          _declaration = new DeclarationNode(type, node);
 				}
+        else if (decl.type == TypeDeclarationNode.Type.VariableDeclaration) {
+          auto node = decl.node;
+          auto type = DeclarationNode.Type.VariableDeclaration;
+          _declaration = new DeclarationNode(type, node);
+        }
 				break;
 		}
 		return false;
---
+++ syntax/static_disambiguation_unit.d
@@ -0,0 +1,63 @@
+/*
+ * static_disambiguation_unit.d
+ *
+ */
+
+module syntax.static_disambiguation_unit;
+
+import syntax.static_if_statement_unit;
+import syntax.static_assert_statement_unit;
+import syntax.type_declaration_unit;
+
+import ast.declaration_node;
+import ast.type_declaration_node;
+
+import lex.lexer;
+import lex.token;
+import logger;
+
+class StaticDisambiguationUnit {
+private:
+	Lexer  _lexer;
+	Logger _logger;
+
+	int    _state;
+
+  enum StaticVariant {
+    StaticIf,
+    StaticAssert,
+    StaticConstructor,
+    StaticDestructor,
+    StaticAttribute
+  }
+
+public:
+
+	this(Lexer lexer, Logger logger) {
+		_lexer  = lexer;
+		_logger = logger;
+	}
+	
+	StaticVariant parse() {
+		Token token = _lexer.pop();
+    _lexer.push(token);
+
+    if (token.type == Token.Type.If) {
+      return StaticVariant.StaticIf;
+    }
+
+    if (token.type == Token.Type.Assert) {
+      return StaticVariant.StaticAssert;
+    }
+
+    if (token.type == Token.Type.This) {
+      return StaticVariant.StaticConstructor;
+    }
+
+    if (token.type == Token.Type.Cat) {
+      return StaticVariant.StaticDestructor;
+    }
+
+    return StaticVariant.StaticAttribute;
+	}
+}
