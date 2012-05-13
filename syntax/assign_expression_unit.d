ba6237cdd1fb9f521909cd52ad18ff8a4669de8b
wilkie <wilkie05@gmail.com>
2012-05-13 03:23:53 -0400

    Adds AssignExpression parsing.

--- syntax/assign_expression_unit.d
+++ syntax/assign_expression_unit.d
@@ -37,11 +37,39 @@ public:
 
 	bool tokenFound(Token token) {
 		switch (token.type) {
+      case Token.Type.Assign:
+			case Token.Type.AddAssign:
+			case Token.Type.SubAssign:
+			case Token.Type.CatAssign:
+			case Token.Type.MulAssign:
+      case Token.Type.DivAssign:
+      case Token.Type.ModAssign:
+      case Token.Type.OrAssign:
+      case Token.Type.AndAssign:
+      case Token.Type.XorAssign:
+      case Token.Type.ShiftLeftAssign:
+      case Token.Type.ShiftRightAssign:
+      case Token.Type.ShiftRightSignedAssign:
+				if (this._state == 1) {
+					this._state = 0;
+					break;
+				}
+
+				// Fall through
+				goto default;
+
 			default:
 				_lexer.push(token);
+				if (this._state == 1) {
+					// Done.
+					return false;
+				}
 				auto expr = (new ConditionalExpressionUnit(_lexer, _logger)).parse;
+
+        _state = 1;
 				break;
 		}
-		return false;
+
+		return true;
 	}
 }
