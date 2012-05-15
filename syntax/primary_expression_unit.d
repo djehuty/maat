4a58bda89597cf09af24489037726ec0749c4c92
wilkie <wilkie05@gmail.com>
2012-05-15 02:04:33 -0400

    Adds ArrayLiteral disambiguation to PrimaryExpression parsing.

--- syntax/primary_expression_unit.d
+++ syntax/primary_expression_unit.d
@@ -5,6 +5,9 @@
 
 module syntax.primary_expression_unit;
 
+import syntax.array_literal_disambiguation_unit;
+import syntax.array_literal_unit;
+
 import lex.lexer;
 import lex.token;
 import logger;
@@ -52,6 +55,13 @@ public:
 				return false;
 			case Token.Type.Identifier:
 				return false;
+      case Token.Type.LeftBracket:
+        // Disambiguate between different array literal types:
+        auto arrayType = (new ArrayLiteralDisambiguationUnit(_lexer, _logger)).parse;
+        if (arrayType == ArrayLiteralDisambiguationUnit.ArrayLiteralVariant.Array) {
+          auto array = (new ArrayLiteralUnit(_lexer, _logger)).parse;
+        }
+        return false;
 			default:
 				break;
 		}
