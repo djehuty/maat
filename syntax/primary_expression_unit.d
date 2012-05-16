2f92e54aa685cd2e4db1b6ac2a157dae0d9ce187
wilkie <wilkie05@gmail.com>
2012-05-16 15:38:08 -0400

    Adds PrimaryExpressions.

--- syntax/primary_expression_unit.d
+++ syntax/primary_expression_unit.d
@@ -49,12 +49,59 @@ public:
 
 		switch (token.type) {
 			case Token.Type.StringLiteral:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
 				_cur_string = token.string;
 				return false;
 			case Token.Type.IntegerLiteral:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
 				return false;
 			case Token.Type.Identifier:
 				return false;
+      case Token.Type.This:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
+        return false;
+      case Token.Type.Null:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
+        return false;
+      case Token.Type.True:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
+        return false;
+      case Token.Type.False:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
+        return false;
+      case Token.Type.Dollar:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
+        return false;
+      case Token.Type.Super:
+        if (_state == 1) {
+          // Error:
+          return false;
+        }
+        return false;
+      case Token.Type.Dot:
+        _state = 1;
+        break;
       case Token.Type.LeftBracket:
         // Disambiguate between different array literal types:
         auto arrayType = (new ArrayLiteralDisambiguationUnit(_lexer, _logger)).parse;
