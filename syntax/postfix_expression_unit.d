5c2bd381012f689b69c5f31df171aae6a96d4cc4
wilkie <wilkie05@gmail.com>
2012-05-16 15:37:54 -0400

    Adds PostfixExpressions.

--- syntax/postfix_expression_unit.d
+++ syntax/postfix_expression_unit.d
@@ -11,6 +11,7 @@ import logger;
 
 import syntax.primary_expression_unit;
 import syntax.index_or_slice_expression_unit;
+import syntax.argument_list_unit;
 
 /*
 
@@ -57,6 +58,49 @@ public:
         }
 
         break;
+
+      case Token.Type.Identifier:
+        if (_state == 2) {
+          _state = 1;
+        }
+        else if (_state == 1) {
+          // Error: Expect a dot, not an identifier.
+        }
+        else if (_state == 0) {
+          goto default;
+        }
+        break;
+
+      case Token.Type.Dot:
+        if (_state == 1) {
+          _state = 2;
+        }
+        else if (_state == 0) {
+          goto default;
+        }
+        break;
+
+      case Token.Type.Decrement:
+        if (_state == 1) {
+          // Done.
+          return false;
+        }
+        goto default;
+
+      case Token.Type.Increment:
+        if (_state == 1) {
+          // Done.
+          return false;
+        }
+        goto default;
+
+      case Token.Type.LeftParen:
+        if (_state == 1) {
+          auto args = (new ArgumentListUnit(_lexer, _logger)).parse;
+          _state = 1;
+          break;
+        }
+        goto default;
 
 			default:
 				_lexer.push(token);
